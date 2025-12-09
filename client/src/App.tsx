8f3ce1b548f156dfd664202c726f748bc3352ba0
KeremVona <keremvona@gmail.com>
2025-12-09 20:44:05 +0300

    handled the initial routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Game from "./pages/Game";
+import { Navigate } from "react-router";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/game" element={<Game />} />
+          <Route path="/" element={<Navigate to="/game" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
